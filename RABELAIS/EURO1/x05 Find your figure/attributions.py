70d75cc9420ce4a2339946c78c1c776542a552ce
UGL1 <ugli@mailo.com>
2024-01-07 22:01:52 +0100

    up

---
+++ RABELAIS/EURO1/x05 Find your figure/attributions.py
@@ -0,0 +1,7 @@
+lst = ["ONE","TWO","THREE","FOUR","FIVE","SIX","SEVEN","EIGHT","NINE"]
+lst.sort()
+for i in range(len(lst)):
+    print(i+1,lst[i])
+
+
+
--- RABELAIS/NSI2/018-Gestion des processus et ressources/XP/proc_linux.py
+++
@@ -1,7 +0,0 @@
-from os import getpid
-
-pid = str(getpid())
-print(f'----------------------------------------start process {pid}')
-for i in range(1000):
-    print(f'process {pid}\t\t value {i}')
-print(f'________________________________________end process {pid}')
